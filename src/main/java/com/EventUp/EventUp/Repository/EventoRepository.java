484792ab005d194f871e1754a495fd84e1a40891
ThaVieiras <tdetatu@gmail.com>
2025-11-26 16:02:23 -0300

    Create EventoRepository

--- src/main/java/com/EventUp/EventUp/Repository/EventoRepository.java
+++ src/main/java/com/EventUp/EventUp/Repository/EventoRepository.java
@@ -1,4 +1,10 @@
 package com.EventUp.EventUp.Repository;
 
-public class EventoRepository {
+import com.EventUp.EventUp.Model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+
 }
