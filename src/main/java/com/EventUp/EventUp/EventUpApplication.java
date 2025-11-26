392f522c8c18bd6d29c5d7b36ac878ed57f2e0de
Weslley Rocha <weslley.nx@gmail.com>
2025-11-26 14:19:54 -0300

    Fix: Atualizando .gitignore e limpando cache

---
+++ src/main/java/com/EventUp/EventUp/EventUpApplication.java
@@ -0,0 +1,13 @@
+package com.EventUp.EventUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EventUp/EventUp/EventUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventUp.EventUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
