a6a932c6b0581ddbba6e181f49a6b102ccb96f0d
Weslley Rocha <weslley.nx@gmail.com>
2025-12-04 14:07:22 -0300

    Refatoração Repository

--- src/main/java/com/EventUp/EventUp/Repository/UsuarioRepository.java
+++ src/main/java/com/EventUp/EventUp/Repository/UsuarioRepository.java
@@ -1,9 +1,9 @@
 package com.EventUp.EventUp.Repository;
 
 import com.EventUp.EventUp.Model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository  extends JpaRepository<Usuario,Long> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 }
