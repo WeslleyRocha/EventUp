9cf2263b6937fd02cd259775337410784d8f0507
Weslley Rocha <weslley.nx@gmail.com>
2025-12-07 16:06:52 -0300

    Remove Locais.java / LocaisRepository.java

--- src/main/java/com/EventUp/EventUp/Repository/LocaisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EventUp.EventUp.Repository;
-
-import com.EventUp.EventUp.Model.Locais;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocaisRepository  extends CrudRepository<Locais, Long> {
-}
