0dfc25a14c9d7ca1a65db4a8d9d01edbb5aa98fe
Weslley Rocha <weslley.nx@gmail.com>
2025-12-07 17:17:04 -0300

    Refatorando tabela Evento.

--- src/main/java/com/EventUp/EventUp/Model/Evento.java
+++ src/main/java/com/EventUp/EventUp/Model/Evento.java
@@ -1,6 +1,14 @@
 package com.EventUp.EventUp.Model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -21,7 +29,7 @@ public class Evento implements Serializable {
     @Column(name = "descricao_detalhada", columnDefinition = "TEXT")
     private String descricaoDetalhada;
 
-    @Column(name = "data_hora_inicio", nullable = false)
+    @Column(name = "data_hora_inicio")
     private LocalDateTime dataHoraInicio;
 
     @Column(name = "data_hora_fim")
@@ -34,14 +42,40 @@ public class Evento implements Serializable {
     private String urlImagemDestaque;
 
     @Column(name = "gratuito")
-    private Boolean gratuito;
+    private Boolean gratuito = true;
 
     @Column(name = "valor_ingresso")
     private Double valorIngresso;
 
-    @ManyToOne
-    @JoinColumn(name = "id_local", nullable = false)
-    private Locais local;
+    @Column(name = "local_evento")
+    private String local_evento;
+
+    @Column(name = "evento_online")
+    private Boolean evento_online = false;
+
+    @Column(name = "link_evento_online")
+    private String link_evento_online;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "info_estacionamento")
+    private String info_estacionamento;
+
+    @Column(name = "info_transporte_publico")
+    private String info_transporte_publico;
+
+    @Column(name = "info_acessibilidade")
+    private String info_acessibilidade;
 
     @ManyToOne
     @JoinColumn(name = "id_categoria", nullable = false)
@@ -57,6 +91,22 @@ public class Evento implements Serializable {
 
     public void setNomeEvento(String nomeEvento) {
         this.nomeEvento = nomeEvento;
+    }
+
+    public Boolean getEvento_online() {
+        return evento_online;
+    }
+
+    public void setEvento_online(Boolean evento_online) {
+        this.evento_online = evento_online;
+    }
+
+    public String getLink_evento_online() {
+        return link_evento_online;
+    }
+
+    public void setLink_evento_online(String link_evento_online) {
+        this.link_evento_online = link_evento_online;
     }
 
     public String getDescricaoDetalhada() {
@@ -115,12 +165,68 @@ public class Evento implements Serializable {
         this.valorIngresso = valorIngresso;
     }
 
-    public Locais getLocal() {
-        return local;
+    public String getLocal_evento() {
+        return local_evento;
     }
 
-    public void setLocal(Locais local) {
-        this.local = local;
+    public void setLocal_evento(String local_evento) {
+        this.local_evento = local_evento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getInfo_estacionamento() {
+        return info_estacionamento;
+    }
+
+    public void setInfo_estacionamento(String info_estacionamento) {
+        this.info_estacionamento = info_estacionamento;
+    }
+
+    public String getInfo_transporte_publico() {
+        return info_transporte_publico;
+    }
+
+    public void setInfo_transporte_publico(String info_transporte_publico) {
+        this.info_transporte_publico = info_transporte_publico;
+    }
+
+    public String getInfo_acessibilidade() {
+        return info_acessibilidade;
+    }
+
+    public void setInfo_acessibilidade(String info_acessibilidade) {
+        this.info_acessibilidade = info_acessibilidade;
     }
 
     public Categoria getCategoria() {
@@ -141,6 +247,4 @@ public class Evento implements Serializable {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
-
 }
